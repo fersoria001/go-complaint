484f557daa69d14d5c78b301da47d2d6635a5e8b
Fernando Soria <bercho001@gmail.com>
2024-07-03 06:46:43 -0300

    modified an endpoint

--- presenter/src/lib/csrf.ts
+++ presenter/src/lib/csrf.ts
@@ -1,6 +1,6 @@
 
 export async function csrf(): Promise<string> {
-  const response = await fetch("https://go-complaint-server-latest.onrender.com", {
+  const response = await fetch("http://3.143.110.143:5555", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
