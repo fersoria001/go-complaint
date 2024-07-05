08a402ffc09ab2f5ef862b078a2536a7a38774a3
Fernando Soria <bercho001@gmail.com>
2024-07-04 22:40:10 -0300

    back to /

--- presenter/vite.config.ts
+++ presenter/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./index.html",
+  base: "/",
   plugins: [react(),TanStackRouterVite(),],
   css: {
     postcss: {
