a7401aa7b9e4baf5e5ad19e189343391b464caa5
Fernando Soria <bercho001@gmail.com>
2024-07-04 22:35:07 -0300

    base changed to index.html

--- presenter/vite.config.ts
+++ presenter/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://www.go-complaint.com/",
+  base: "./index.html",
   plugins: [react(),TanStackRouterVite(),],
   css: {
     postcss: {
