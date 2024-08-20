47a74a44d5c6765ee2f319d8cd9c4032eef1ed13
Fernando Soria <bercho001@gmail.com>
2024-08-20 09:37:58 -0300

    The images of the terciary section in the main page were resized and the component adjusted to look a little bit better.

--- presentation/src/components/main/TerciarySection.tsx
+++ presentation/src/components/main/TerciarySection.tsx
@@ -10,16 +10,18 @@ const TerciarySection: React.FC = () => {
                         Register your enterprises and
                         invite people to work with you!
                     </p>
-                    <div className="p-5 md:px-36 lg:px-5 md:text-center lg:text-start">
-                        <div className="relative w-full h-[200px] md:mx-auto md:w-[400px] md:h-[300px] shadow-md rounded-md">
-                            <Image
-                                src={'/main7.png'}
-                                alt={'Image of Go complaint profile'}
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                fill
-                            //className="object-cover"
-                            />
-                        </div>
+                    <div className="md:p-1 md:text-center lg:text-start">
+                        <Image
+                            src={'/main3.png'}
+                            alt={'Image of Go complaint profile'}
+                            sizes="100vw"
+                            style={{
+                                width: '100%',
+                                height: 'auto',
+                            }}
+                            width={600}
+                            height={300}
+                        />
                     </div>
                 </div>
                 <div>
@@ -27,40 +29,44 @@ const TerciarySection: React.FC = () => {
                         Assign them duties and let them
                         take care of customers support.
                     </p>
-                    <div className="p-5 md:px-36 lg:px-5 md:text-center lg:text-start">
-                        <div className="relative w-full h-[200px] md:mx-auto md:w-[400px] md:h-[300px] shadow-md rounded-md">
+                    <div className="md:p-1 md:text-center lg:text-start">
                             <Image
-                                src={'/main6.png'}
+                                src={'/main5.png'}
                                 alt={'Image of Go complaint profile'}
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                fill
-                            //className="object-cover"
+                                sizes="100vw"
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                }}
+                                width={600}
+                                height={300}
                             />
-                        </div>
                     </div>
                 </div>
                 <div>
-                    <p className="mb-4  px-5 md:text-center lg:text-start text-lg text-gray-700 md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+                    <p className="mb-4 px-5 md:text-center lg:text-start text-lg text-gray-700 md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
                         Provide them feedback so that everyone can
                         improve its attention.
                     </p>
-                    <div className="p-5 md:px-36 lg:px-5 md:text-center lg:text-start">
-                        <div className="relative w-full h-[200px] md:mx-auto md:w-[400px] md:h-[300px] shadow-md rounded-md">
+                    <div className="md:p-1 md:text-center lg:text-start">
                             <Image
                                 src={'/main4.png'}
                                 alt={'Image of Go complaint profile'}
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                fill
-                                className="object-cover"
+                                sizes="100vw"
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                }}
+                                width={600}
+                                height={300}
                             />
-                        </div>
                     </div>
                 </div>
             </div>
             <div className="bg-blue-500 h-[80vh]  w-full  overflow-hidden">
                 <div className="container">
                     <div className="wave">
-                        <div className="flex flex-col z-100 absolute left-10 2xl:-top-20 xl:-top-5">
+                        <div className="flex flex-col z-100 absolute left-10 2xl:-top-20 xl:-to">
                             <h1 className="px-2 text-center mb-4 text-gray-700 font-medium text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
                                 At Go Complaint we care about customers
                             </h1>
